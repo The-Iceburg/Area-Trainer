d9dd235c0b9a92c7989541b435a4fa5d1a6e2513
Ormatist <thescientist771.mail@gmail.com>
2022-05-26 21:50:36 +0100

    v0.1.4
    Implemented #2

--- AreaTrainer.py
+++ AreaTrainer.py
@@ -1,6 +1,10 @@
+import random
+import math
+
+from matplotlib import widgets
 from passwordstrengthchecker import passwordcheck as pc
 
-tilda = ["~"]
+tilda,possanswers = ["~"],[1,2,3,4]
 
 def menu():
 
@@ -70,13 +74,80 @@ def menu():
         menu()
 
 def shapemen():
-    choice = int(input("##############################################\nPlease select your option from the list below:\n1. Triangle\n2. Rectangle\n3. Circle\n4. Quit (Logout)\n##############################################\n"))
+    choice = int(input("##############################################\nPlease select your option from the list below:\n1. Triangle △\n2. Rectangle ▯\n3. Circle ○\n4. Quit (Logout)\n##############################################\n"))
     if choice == 1:
-        print("Triangle")
+
+        perpheight = random.randint(2,15)
+        width = random.randint(2,15)
+
+        answer = perpheight * width / 2
+
+        for i in range(4):
+            
+            perpheightpred = random.randint(2,15)
+            widthpred = random.randint(2,15)
+
+            answerpred = perpheightpred * widthpred / 2
+
+            possanswers[i] = answerpred
+
+        answerno = random.randint(0,3)
+        possanswers[answerno] = answer
+
+        question = int(input("##############################################\n△ - Triangle\n\nPerpendicular Height = " + str(perpheight) +"\nWidth = " + str(width) + "\n\n1. " + str(possanswers[0])+ " 2. " + str(possanswers[1]) + " 3. " + str(possanswers[2]) + " 4. " + str(possanswers[3]) + "\n##############################################\n"))
+        if question == answerno + 1:
+            print("correct")
+        else:
+            print("incorrect")
+
     elif choice == 2:
-        print("Rectangle")
+        
+        height = random.randint(2,15)
+        width = random.randint(2,15)
+
+        answer = height * width
+
+        for i in range(4):
+            
+            heightpred = random.randint(2,15)
+            widthpred = random.randint(2,15)
+
+            answerpred = heightpred * widthpred
+            possanswers[i] = answerpred
+
+        answerno = random.randint(0,3)
+        possanswers[answerno] = answer
+
+        question = int(input("##############################################\n▯ - Rectangle\n\nWidth = " + str(width) +"\nHeight = " + str(height) +"\n\n1. " + str(possanswers[0])+ " 2. " + str(possanswers[1]) + " 3. " + str(possanswers[2]) + " 4. " + str(possanswers[3]) + "\n##############################################\n"))
+        if question == answerno + 1:
+            print("correct")
+        else:
+            print("incorrect")
+
     elif choice == 3:
-        print("Circle")
+        
+        radius = random.randint(2,15)
+
+        answer = math.pi * (radius**2)
+        answer = round(answer, 2)
+
+        for i in range(4):
+            
+            radiuspred = random.randint(2,15)
+
+            answerpred = math.pi * (radiuspred**2)
+            answerpred = round(answerpred, 2)
+            possanswers[i] = answerpred
+
+        answerno = random.randint(0,3)
+        possanswers[answerno] = answer
+
+        question = int(input("##############################################\n○ - Circle\n\nRadius = " + str(radius) +"\n\n1. " + str(possanswers[0])+ " 2. " + str(possanswers[1]) + " 3. " + str(possanswers[2]) + " 4. " + str(possanswers[3]) + "\n\nGive your answer rounded to 2.dp\n##############################################\n"))
+        if question == answerno + 1:
+            print("correct")
+        else:
+            print("incorrect")
+
     elif choice == 4:
         print("##############################################\nSuccesfully logged out!\nThanks for using the area trainer!\n##############################################\n")
         exit()
